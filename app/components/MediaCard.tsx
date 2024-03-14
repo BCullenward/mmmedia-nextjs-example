587dcea4310f10c0f4d427320d08f2e833660f7b
BCullenward <59399934+BCullenward@users.noreply.github.com>
2024-03-14 15:33:44 -0500

    add Favorites functionality

--- app/components/MediaCard.tsx
+++ app/components/MediaCard.tsx
@@ -3,6 +3,8 @@ import { Button } from "@/components/ui/button";
 import { Heart, PlayCircle } from "lucide-react";
 import PlayVideoModal from "./PlayVideoModal";
 import { useState } from "react";
+import { addToWatchlist, removeFromWatchlist } from "../action";
+import { usePathname } from "next/navigation";
 
 interface iAppProps {
   title: string;
@@ -29,6 +31,7 @@ export function MediaCard({
 }: iAppProps) {
   const [open, setOpen] = useState(false);
 
+  const pathName = usePathname();
   return (
     <>
       <button onClick={() => setOpen(true)} className="-mt-20">
@@ -36,13 +39,17 @@ export function MediaCard({
       </button>
       <div className="right-5 top-5 absolute z-10">
         {watchList ? (
-          <form>
+          <form action={removeFromWatchlist}>
+            <input type="hidden" name="watchlistId" value={watchListId} />
+            <input type="hidden" name="pathname" value={pathName} />
             <Button variant="outline" size="icon">
               <Heart className="w-4 h-4 text-red-500" />
             </Button>
           </form>
         ) : (
-          <form>
+          <form action={addToWatchlist}>
+            <input type="hidden" name="movieId" value={movieId} />
+            <input type="hidden" name="pathname" value={pathName} />
             <Button variant="outline" size="icon">
               <Heart className="w-4 h-4" />
             </Button>
